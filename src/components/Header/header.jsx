02592ff8ca346867c323862e9d45782820840cde
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-20 14:46:04 +0100

    place to yarn or npm

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 function Header(){
     // const imageStar = require('../../images/');
     const imageStar = require('../../images/icon_star.svg');
+    const titlePage = "Home";
     return (
         <div className="header">
-            <h1>Header</h1>
-            <img src="{imageStar}" alt="l'icon star" />
+            <h1>{titlePage}</h1>
+            {/* <img src={imageStar} alt="l'icon star" /> */}
         </div>
     )
 }
