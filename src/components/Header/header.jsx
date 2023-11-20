09e69234582c073b42b0d087957b72c6aa4c2c7e
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-20 14:21:08 +0100

    place a image in header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 function Header(){
+    // const imageStar = require('../../images/');
+    const imageStar = require('../../images/icon_star.svg');
     return (
         <div className="header">
             <h1>Header</h1>
+            <img src="{imageStar}" alt="l'icon star" />
         </div>
     )
 }
