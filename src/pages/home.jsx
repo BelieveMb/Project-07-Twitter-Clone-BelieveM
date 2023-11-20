02efb2a1f7bd9a979b62e2e1e65041ef3b5e4213
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-20 11:49:21 +0100

    create de folder for composants

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/header';
+
+function Home() {
+  return (
+    <main className="timeline">
+      <Header />
+      <h1>Timeline here</h1>
+    </main>
+  );
+}
+
+export default Home;
