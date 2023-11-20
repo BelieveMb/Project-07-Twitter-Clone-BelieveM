7eec861f61f9e8f6edd4ba95f3f162d05eaef229
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-20 12:16:25 +0100

    end place the composants of home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/header';
 import TweetText from '../components/Tweet/TweetText/TweetText';
 import EditorTweet from '../components/EditorTweet/EditorTweet';
+import TweetImage from '../components/Tweet/Tweetimage/TweetImage';
 
 function Home() {
   return (
@@ -9,7 +10,9 @@ function Home() {
       <Header />
       <EditorTweet />
       <TweetText />
-
+      <TweetImage />
+      <TweetText />
+      <TweetText />
     </main>
   );
 }
