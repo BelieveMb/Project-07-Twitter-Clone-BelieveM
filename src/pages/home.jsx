098b4039674b53517b3d24798f547e9fa09479d7
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-05 12:15:31 +0100

    start create sidabar component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Timeline from '../components/timeline/Timeline';
 import SideBar from '../components/sidebar/SideBar';
+import RightBar from '../components/rightbar/RightBar';
 
 
 function Home() {
   return (
-    <main className="timeline">
-      <Header />
-      <EditorTweet />
-      <Tweet numberTweet={0} />
-      <Tweet numberTweet={1} />
-      <Tweet numberTweet={2} />
-      <Tweet numberTweet={3} />
-    </main>
+    <>
+      <SideBar />
+      <main className="timeline">
+        <Timeline />
+      </main>
+      <RightBar />
+
+    </>
+    
   );
 }
 
