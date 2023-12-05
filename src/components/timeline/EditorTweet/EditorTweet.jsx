ae8e0cd689cae0c9a501e886c160a1ac9092295c
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-05 12:06:10 +0100

    link for image

--- src/components/timeline/EditorTweet/EditorTweet.jsx
+++ src/components/timeline/EditorTweet/EditorTweet.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import imageAvatar from '../../images/profile-photo.png';
-import imageMedia from "../../images/Media.png";
-import imageGif from "../../images/Gif.png";
-import imagePoll from "../../images/Poll.png";
-import imageSmile from "../../images/Emoji.png";
-import imageSchedule from "../../images/Schedule.png";
+import imageAvatar from '../../../images/profile-photo.png';
+import imageMedia from "../../../images/Media.png";
+import imageGif from "../../../images/Gif.png";
+import imagePoll from "../../../images/Poll.png";
+import imageSmile from "../../../images/Emoji.png";
+import imageSchedule from "../../../images/Schedule.png";
 
 
 function EditorTweet() {
