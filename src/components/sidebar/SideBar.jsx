b5677442d07e30b4301bc91918067bd289db44c4
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-05 12:15:31 +0100

    start create sidabar component

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import imageAvatar from '../../../images/profile-photo.png';
+import imageMedia from "../../../images/Media.png";
+import imageGif from "../../../images/Gif.png";
+import imagePoll from "../../../images/Poll.png";
+import imageSmile from "../../../images/Emoji.png";
+import imageSchedule from "../../../images/Schedule.png";
 
 function SideBar() {
   return (
-    <div className='sidebar'> SideBar</div>
+    <div className='sidebar'> 
+
+    </div>
   )
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Timeline from '../components/timeline/Timeline';
 import SideBar from '../components/sidebar/SideBar';
+import RightBar from '../components/rightbar/RightBar';
 
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <main className="timeline">
         <Timeline />
       </main>
+      <RightBar />
 
     </>
     
