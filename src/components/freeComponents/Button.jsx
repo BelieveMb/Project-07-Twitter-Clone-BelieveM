20d23ac87612e89d92e5817647e7c37c20cc1f82
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-07 00:47:17 +0100

    profilPhoto in bannerProfile

---
+++ src/components/freeComponents/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Button({btnContent}) {
+  return (
+    <a className='btnFollow'>{btnContent}</a>            
+
+  )
+}
+
+export default Button
--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TitleProfile from './titleProfile'
+import BannerProfile from './bannerProfile'
 
 function UserProfile() {
   return (
     <main className="timeline">
       <TitleProfile />
+      <BannerProfile />
     </main>
   )
 }
