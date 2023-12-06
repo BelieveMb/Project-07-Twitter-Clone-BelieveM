274d0d3a54fbc303da3fc5bd451556f7bf752468
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-06 23:37:59 +0100

    start page userProfil

--- src/pages/pageError.jsx
+++ src/pages/pageError.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SideBar from '../components/sidebar/SideBar'
 import RightBar from '../components/rightbar/RightBar'
+import Layout from '../components/layout'
 
 function PageError() {
   return (
     <>
       <SideBar />
-      <h1>PageError</h1>
+            <Layout>
+              <h1>Le site est en construction</h1>
+            </Layout>
       <RightBar />
     </>
   )
