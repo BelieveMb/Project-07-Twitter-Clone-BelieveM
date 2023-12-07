d40491a87c13d672180b0d1e16fde4eb3623dcd2
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-07 11:40:08 +0100

    set show title profile by idUser

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -3,11 +3,14 @@ import TitleProfile from './titleProfile'
 import BannerProfile from './bannerProfile'
 import ContentProfile from './contentProfile'
 import NavProfile from './navProfile'
+import { useParams } from 'react-router-dom'
 
 function UserProfile() {
+  const { id } = useParams();
+
   return (
     <main className="timeline">
-      <TitleProfile />
+      <TitleProfile key={id}/>
       <BannerProfile />
       <ContentProfile />
       <NavProfile />
