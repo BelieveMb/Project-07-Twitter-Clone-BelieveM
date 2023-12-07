da39b01ad8c3b55a700813be043c326decc863ad
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-07 02:29:13 +0100

    end ContentProfile

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TitleProfile from './titleProfile'
 import BannerProfile from './bannerProfile'
+import ContentProfile from './contentProfile'
 
 function UserProfile() {
   return (
     <main className="timeline">
       <TitleProfile />
       <BannerProfile />
+      <ContentProfile />
     </main>
   )
 }
--- src/components/userProfile/contentProfile.jsx
+++ src/components/userProfile/contentProfile.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-function contentProfile() {
+function ContentProfile() {
   return (
-    <div>contentProfile</div>
+    <div className='contentProfile'>
+      <div className="titleName">
+        <h2>
+            CNN
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+              <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+            </svg>
+        </h2>
+        <span>@CNN</span>
+      </div>
+
+      <p className='linkdeco'>
+        It’s our job to <a href="">#GoThere</a> & tell the most difficult stories. For breaking news, follow 
+        <a href="">@CNNBRK</a> and download our app <a href="">http://cnn.com/apps</a> 
+      </p>
+      <p className='contentDetail linkdeco'>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 7H16C18.7614 7 21 9.23858 21 12C21 14.7614 18.7614 17 16 17H14M10 7H8C5.23858 7 3 9.23858 3 12C3 14.7614 5.23858 17 8 17H10M8 12H16" stroke="#afacac" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+        <a href="">cnn.com </a>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#afacac"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#afacac" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+        A rejoint Twitter en février 2007
+      </p>
+
+      <p className='contentDetail linkdeco'>
+        <span><b>1 086</b>  abonnements </span>       <span><b>62 M </b>   abonnés</span>
+      </p>
+    </div>
   )
 }
 
-export default contentProfile
+export default ContentProfile
